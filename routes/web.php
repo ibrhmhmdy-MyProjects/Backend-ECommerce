96b1e683f8e4a9a7055fd25637e99eddba2a8877
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-10-23 14:57:17 +0300

    Add Products Table and Migrate

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
-Route::get('/dashboard',function(){
+// Route::AdminPanel Side
+Route::get('/admin/dashboard',function(){
     return view('admin.index');
 });
